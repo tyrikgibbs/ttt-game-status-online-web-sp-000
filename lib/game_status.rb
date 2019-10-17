cb3044dfed3a8abe1ebdf9b14575184c40ec9efe
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-17 23:55:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,5 +5,10 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def won(board)
-  
+  top_row_win = [0,1,2]
+
+
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+  end
 end
