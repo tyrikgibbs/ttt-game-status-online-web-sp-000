610eedce6023c1954b15e53868cdb24fefe30322
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-18 01:56:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,10 +63,10 @@ def winner?(board)
   index = won?(board)
    if index == false
      return nil
-   else
+   elsif
      if board[index[0]] == "X"
        return "X"
-     else
+   else
        return "O"
      end
    end
