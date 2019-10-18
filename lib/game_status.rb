1913f250d257f4b47d1dfc419190d892c58031a6
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-18 01:08:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
             return win_combo
           elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
             return win_combo
-          end
+      end
 
         return false
-      end
+end
