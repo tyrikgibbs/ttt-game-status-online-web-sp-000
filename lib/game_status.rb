3c3c3691041c284b3c5900013b58f99d4161832a
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-18 01:07:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,12 +24,12 @@ def won?(board)
       position_1 = board[index_0]
       position_2 = board[index_1]
       position_3 = board[index_2]
-      
+
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
             return win_combo
           elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
             return win_combo
           end
-        
+
         return false
       end
